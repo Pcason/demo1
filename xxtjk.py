a6c622b797345dad059cec928bcc1c70009f476b
Pcason <101849887+Pcason@users.noreply.github.com>
2023-03-29 15:07:27 +0800

    Update xxtjk.py

--- xxtjk.py
+++ xxtjk.py
@@ -1,16 +1,24 @@
 # coding:utf-8
+"""
+项目名称：学习通积分商城监控
+变量：xxtck。 抓cookie中stw，例如‘stw=xxxxxxx’
+定时：4小时一次，有可兑换商品时自动推送，没有则不推送。
+项目地址：https://github.com/Pcason/demo1/blob/main/xxtjk.py
+"""
+import os
 from datetime import datetime
 import notify
+
 import requests
 
 
-def get_content():
+def get_content(xxtck):
     content = ""
     url = 'http://hnqndaxuexi.dahejs.cn/stw/product/list'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6309001c) XWEB/6500",
         "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": "stw=8d56d00c-d275-4963-bbbd-095e5cdf5b7e",
+        "Cookie": xxtck,
     }
     data = {
         "pageNumber": "1",
@@ -36,11 +44,12 @@ def get_content():
 
 
 def main():
-    content = get_content()
-    if content != "":
-        notify.pushplus_bot('学习通积分商城库存监控', content)
-    print('暂无可兑换商品！')
-
+    ck_list = os.getenv('xxtck').split('\n')
+    for xxtck in ck_list:
+        content = get_content(xxtck)
+        if content != "":
+            notify.pushplus_bot('学习通积分商城库存监控', content)
+        print('暂无可兑换商品！')
 
 
 if __name__ == '__main__':
