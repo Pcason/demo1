6e0d05f5823e1ab00351eee4e1f3716912301131
Pcason <101849887+Pcason@users.noreply.github.com>
2023-07-05 19:20:55 +0800

    新增登录功能直接提交手机号即可

--- zsoh.py
+++ zsoh.py
@@ -1,25 +1,21 @@
 # coding:utf-8
 """
 项目名称：掌上瓯海APP
-抓包vapp.tmuyun.com.cn域名下请求头X-SESSION-ID的值
+注册登录后设置登录密码为123456，填写手机号即可！介意者勿用！！！
 环境变量ZSOH，多账号换行隔开
 作者：cason
 项目地址：https://github.com/Pcason/demo1/blob/main/zsoh.py
 """
 import os
 import time
-import base64
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
-from io import BytesIO
 import requests
 import notify
 import hashlib
 
 
 class ZhangShangOHai:
-    def __init__(self, session_id):
-        self.session_id = session_id
+    def __init__(self, phone):
+        self.session_id = self.login(phone)
 
     @staticmethod
     def signature(str, str2, str3, str4, str5):
@@ -63,7 +59,8 @@ class ZhangShangOHai:
         for id in id_list:
             data = {
                 'channel_article_id': str(id),
-                'content': 'good'
+                'content': 'good',
+                'sort_type': "0"
             }
             headers = self.get_headers(url)
             res = requests.post(url, data=data, headers=headers)
@@ -118,7 +115,7 @@ class ZhangShangOHai:
         res = requests.get(url, params=params, headers=headers)
         article_list = res.json()["data"]["article_list"]
         id_list = [x['id'] for x in article_list]
-        return id_list[:10]
+        return id_list[0:10]
 
     def get_sign(self):
         url = 'https://vapp.tmuyun.com/api/user_mumber/sign'
@@ -137,35 +134,55 @@ class ZhangShangOHai:
         points = res.json().get('data').get('rst').get('total_integral')
         return phone, points
 
+    def login(self, phone):
+        """
+        登录
+        Returns:
+        """
+        url = 'https://passport.tmuyun.com/web/oauth/credential_auth'
+        data = {
+            'client_id': '10032',
+            'password': "OqGdlgP9MjbfnX5oHra/BvZN/FUAnAD3XE18HkEtiOemMpSbZL4MFgwkwreSx3CODMbvcPC4NV1g04hvWNnlYQW9zdaVeczdJxyYjuAtxUKJ18mVZc0imwGIR34tk5rG5l9mhZqDxYgH4I7BSA2n2ZL10GbbmHDSFmdP5VV/0Cc=",
+            # encode_password('123456'),
+            'phone_number': str(phone)
+        }
+        headers = {
+            'User-Agent': 'ANDROID;13;10032;5.0.0;1.0;null;M2012K11AC',
+            'X-REQUEST-ID': '1612a329-cd31-4fab-a08d-57cd3db0082d',
+            'X-SIGNATURE': 'ffbc5dc4fb8e08cef7c549ebfb7f556a2998c1579809f6961cee749cd3176198',
+            'COOKIE': 'SESSION=NWE5MTEwYzMtYzc0Mi00NGQyLTgzNzMtMWM2YWMxYzY4NThh; Path=/; HttpOnly; SameSite=Lax'
+        }
+        res = requests.post(url, data=data, headers=headers)
+        code = res.json().get("data").get("authorization_code").get("code")
+        url = "https://vapp.tmuyun.com/api/zbtxz/login"
+        headers = {
+            'X-SESSION-ID': "646b37cf7dee053719530a5f",
+            'X-REQUEST-ID': '9357add2-f91a-4dbb-8a77-988baae5b97d',
+            'X-TIMESTAMP': str(int(time.time() * 1000)),
+            'X-SIGNATURE': '',
+            'X-TENANT-ID': '78',
+            'User-Agent': '5.0.0;00000000-67e8-b8c7-0000-0000476ca5a0;Xiaomi M2012K11AC;Android;13;Release',
+            'X-ACCOUNT-ID': '64643f2e2cd91836ea7e0954'
+        }
+        sign = self.signature(url.split('.com')[1], headers.get('X-SESSION-ID'), headers['X-REQUEST-ID'],
+                              headers['X-TIMESTAMP'],
+                              headers['X-TENANT-ID'])
+        headers['X-SIGNATURE'] = sign
 
-def encode_password(password):
-    PASSPORT_PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD6XO7e9YeAOs+cFqwa7ETJ+WXizPqQeXv68i5vqw9pFREsrqiBTRcg7wB0RIp3rJkDpaeVJLsZqYm5TW7FWx/iOiXFc+zCPvaKZric2dXCw27EvlH5rq+zwIPDAJHGAfnn1nmQH7wR3PCatEIb8pz5GFlTHMlluw4ZYmnOwg+thwIDAQAB";
-    bArr = password.encode('utf-8')
-    decode_password = base64.encodebytes(a(bArr, PASSPORT_PUBLIC_KEY)).decode('utf-8')
-    return decode_password
-
-
-def a(bArr, k):
-    # 读取公钥
-    key = RSA.import_key(base64.decodebytes(k.encode('utf-8')))
-    # 使用公钥进行加密
-    cipher = PKCS1_v1_5.new(key)
-    in_data = BytesIO(bArr)
-    out_data = BytesIO()
-    while True:
-        chunk = in_data.read(117)
-        if len(chunk) == 0:
-            break
-        out_data.write(cipher.encrypt(chunk))
-    return out_data.getvalue()
-
+        data = {
+            'code': code,
+            'type': '-1'
+        }
+        r = requests.post(url, data=data, headers=headers)
+        session_id = r.json().get('data').get('session').get('id')
+        return session_id
 
 
 def main():
     content = ''
-    token_list = os.getenv('ZSOH').split('\n')
-    for token in token_list:
-        hai = ZhangShangOHai(token)
+    phone_list = os.getenv('ZSOH').split('\n')
+    for phone in phone_list:
+        hai = ZhangShangOHai(phone)
         hai.get_sign()
         hai.service()
         id_list = hai.get_title_list()
